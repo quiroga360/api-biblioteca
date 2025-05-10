f68d904c79c3bfacebfc828dbf2dbe6244760199
Pablo Quiroga <quirogalogins@gmail.com>
2025-05-09 22:32:53 -0300

    crud de autor finalizado

--- src/controllers/AutorController.js
+++ src/controllers/AutorController.js
@@ -33,7 +33,25 @@ class AutorController {
         }
     }
 
+    static async atualizarAutorPorId(req, res) {
+        try {
+            const id = req.params.id;
+            await Autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: `Autor atualizado com sucesso.` });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 
+    static async deletarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            await Autor.findByIdAndDelete(id);
+            res.status(200).json({ message: `Autor deletado com sucesso.` });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 
 }
 
--- src/routes/autoresRoute.js
+++ src/routes/autoresRoute.js
@@ -5,5 +5,7 @@ const AutorController = require('../controllers/AutorController.js');
 router.post('/autores', (req, res) => AutorController.criarAutor(req, res));
 router.get('/autores', (req, res) => AutorController.listarAutores(req, res));
 router.get('/autores/:id', (req, res) => AutorController.listarAutorPorId(req, res));
+router.put('/autores/:id', (req, res) => AutorController.atualizarAutorPorId(req, res));
+router.delete('/autores/:id', (req, res) => AutorController.deletarLivroPorId(req, res));
 
 module.exports = router;
